c4234d02a377f3335e47625c0c34131731b7ecde
Nsisong-Akpakpan <93917408+Nsisong-Akpakpan@users.noreply.github.com>
2022-10-20 11:22:26 +0100

    divider bar template

--- src/Templates/Dividerbar.js
+++ src/Templates/Dividerbar.js
@@ -2,7 +2,7 @@ import {Box, Typography, Divider} from "@mui/material";
 function DividerBar() {
     return(
         <div>
-            <Box sx={{justifyContent:"center", height:"303px", color:"white", backgroundColor:"rgba(42, 86, 159, 0.95);", display:"grid", gridGap:"0px", gridTemplateColumns: "0.2fr 0.17px 0.2fr 0.17px 0.2fr 0.17px 0.2fr", alignItems:"center"}}>
+            <Box mt={13} sx={{justifyContent:"center", height:"303px", color:"white", backgroundColor:"rgba(42, 86, 159, 0.95);", display:"grid", gridGap:"0px", gridTemplateColumns: "0.2fr 0.17px 0.2fr 0.17px 0.2fr 0.17px 0.2fr", alignItems:"center"}}>
                 <Box sx={{ border:"1px solid red", height:"97.7px", textAlign:"center"}}>
                     <Typography variant="h2" sx={{fontWeight:"bold"}}>200</Typography>
                     <Typography variant="">Students</Typography>
